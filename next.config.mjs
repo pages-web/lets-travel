38ad3a77e00d6efd26058a0d5c4d8464adf0087c
erxes <dev@erxes.io>
2026-07-06 23:31:38 +0800

    chore: add vercel cli and update next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_ERXES_ENDPOINT: "https://letstravel.next.erxes.io/gateway/graphql",
+    NEXT_PUBLIC_ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6Im1DSjRsQzhRaUdxUDhVN3FsbmhrWCIsImlhdCI6MTc4Mjc4OTYyMX0.2Rq7zGp9oOgHD0cBeFHP47hsqBXSuNpRzl7fQnf1HJc",
+    NEXT_PUBLIC_ERXES_CMS_ID: "6a4b2392d4a990261f49300d",
+    ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6Im1DSjRsQzhRaUdxUDhVN3FsbmhrWCIsImlhdCI6MTc4Mjc4OTYyMX0.2Rq7zGp9oOgHD0cBeFHP47hsqBXSuNpRzl7fQnf1HJc",
+  },
+};
 
 export default withNextIntl(nextConfig);
