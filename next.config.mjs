e40ba4860abad4ca4d7f2dffdea4f36290383eae
erxes <dev@erxes.io>
2026-07-07 00:02:46 +0800

    fix: use default .next output dir

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "dist",
   images: {
     unoptimized: true,
   },
